8913041f4e9fafaca3eef42fc5b17d547f436809
Valerii Kotsulym <rinvalery@gmail.com>
2025-05-13 14:25:19 +0200

    Add version codes. Enable minifying on specific libraries.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -5,4 +7,19 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
+}
+
+val versionProps = Properties().apply {
+    load(File("${rootProject.projectDir}/version.properties").inputStream())
+}
+
+subprojects {
+    afterEvaluate {
+        extensions.findByType(com.android.build.gradle.BaseExtension::class)?.apply {
+            defaultConfig {
+                versionName = versionProps["versionName"].toString()
+                versionCode = versionProps["versionCode"].toString().toInt()
+            }
+        }
+    }
 }
