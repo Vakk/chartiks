a2a171fb9b098e953a877728b2787a9b98d82590
Valerii Kotsulym <rinvalery@gmail.com>
2026-06-02 10:39:42 +0200

    Resolve issues with Gradle 9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.built.in1.kotlin) apply false
 }
 
 val versionProps = Properties().apply {
